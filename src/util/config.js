cbe9f24b3b48ff4ece32462035fa190dcffdf241
Administrator <Administrator@PC201904041813B>
2019-10-11 09:27:55 +0800

    修改-适配B28.exe

--- src/util/config.js
+++ src/util/config.js
@@ -283,12 +283,12 @@ const baseQuestions = [{
             validate: valid.folder // 必填，可以是文件也可以是文件夹
         }, {
             type: 'input',
-            name: 'jsonPath',
+            name: 'languagePath',
             message: '语言包文地址（文件夹）：',
             validate: valid.folder
         }, {
             type: 'input',
-            name: 'baseWritePath',
+            name: 'baseOutPath',
             message: '提取的Excel文件输出地址：',
             default (answers) {
                 return getDirname(answers.baseReadPath);
